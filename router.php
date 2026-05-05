e0861c06b6c300b392e3fe19f46a69c9d5e96c40
UPRAVA SRPSKE KIBERNETIKE <milutingavrilovicbeograd@gmail.com>
2026-05-06 00:16:12 +0200

    Update router.php

--- router.php
+++ router.php
@@ -22,3 +22,89 @@ require $pages[$route];
 #-----u suprotnom poziva default stranicu----------------#
 } else { require_once(ROOT_DIR .'nepostojecabiblioteka.php'); }
 ?>
+
+
+<?php
+#-----nepostojecaklasa.php----------#
+#---nema direktnog pristupa klasi---#
+defined('ROOT_DIR') or exit('No direct script access allowed');
+#---klasa engine up ng1np---#
+class ng1np {
+	#---javna metoda render---#
+	public function render($script) {
+        	$this->_require($script);
+    	}
+	    #---zasticena metoda _require---#
+    	protected function _require() {
+        	require_once func_get_arg(0);
+    	}
+}
+?>
+
+
+<?php
+#----nepostojecabiblioteka.php----#
+#---pozivanje klase---#
+$eng = new ng1np();
+#---varijable---#
+$eng->charset = 'UTF-8';
+$eng->keywords = '0000, 0110, 1001, 1111';
+$eng->author = 'admin';
+$eng->description = 'This is God mode. All about universe and galaxy';
+$eng->title = 'God mode - ng1np';
+#----nizovi sa podacima---#
+$eng->content = array( 
+	array(
+	"proces1" => "PAMETNI",
+	"proces2" => "VIRTUELNI",
+	"proces3" => "SIGURNOSNI",
+	"proces4" => "PROCEDURALNI"
+	),
+	array(
+	"proces1" => "DELJENI",
+    "proces2" => "DEFINITIVNI",
+    "proces3" => "SIGURNOSNI",
+    "proces4" => "VIRTUELNI"
+	),
+	array(
+	"proces1" => "DELJENI",
+    "proces2" => "DEFINITIVNI",
+    "proces3" => "SIGURNOSNI",
+    "proces4" => "VIRTUELNI"
+	),
+	array(
+	"proces1" => "PROCEDURALNI",
+    "proces2" => "PROCEDURALNI",
+    "proces3" => "PAMETNI",
+    "proces4" => "VIRTUELNI"
+	)
+);
+$eng->sidebar = array( 
+        array(
+        "server1" => "USER",
+        "server2" => "ROOT",
+        "server3" => "god"
+        ),
+        array(
+        "server1" => "USER",
+        "server2" => "ROOT",
+        "server3" => "root"
+        ),
+        array(
+        "server1" => "ROOT",
+        "server2" => "USER",
+        "server3" => "root"
+        ),
+        array(
+        "server1" => "ROOT",
+        "server2" => "ROOT",
+        "server3" => "user"
+        )
+);
+?>
+
+
+
+
+#---metoda render---#
+echo $eng->render('up/god.tpl');
